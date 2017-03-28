768002131ddbbea81492f413cf0653e0352c7b2c
Duncan Hall <himself@duncanhall.net>
2017-03-28 21:17:34 +0100

    File input expansion

--- bin/expand-swagger-refs.js
+++ bin/expand-swagger-refs.js
@@ -2,6 +2,7 @@
 'use strict';
 
 const fs = require('fs');
+const path = require('path');
 const { expanded } = require('../index.js')
 const args = process.argv.splice(2);
 const inputFile = args[0];
@@ -16,11 +17,31 @@ fs.access(inputFile, fs.constants.R_OK, (error) => {
     console.error(`Could not read file '${error.path}'. If the file exists, check you have permission to read it.`);
     return process.exit(2);
   } else {
-    console.log('File is fine');
+    const ext = path.extname(inputFile);
+    const basename = path.basename(inputFile, ext);
+    const outputFile = path.join(path.dirname(inputFile), `${basename}-expanded${ext}`);
+    expandFile(inputFile, outputFile)
   }
 });
 
-function expandFile() {
+function expandFile(inputFile, outputFile) {
+  fs.readFile(inputFile, 'utf8', (error, data) => {
+    if (error) {
+      console.error(error);
+    } else {
+      const contents = JSON.parse(data);
+      writeFile(outputFile, contents)
+    }
+  })
+}
 
+function writeFile(outputFile, contents) {
+  fs.writeFile(outputFile, JSON.stringify(contents), (error) => {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(`Expanded file written to ${outputFile}`);
+    }
+  })
 }
 
--- test/index.js
+++ test/index.js
@@ -1,17 +1,24 @@
 'use strict';
 
 const assert = require('assert');
-const testData = require('./data/input');
 
 describe('Expand Swagger Refs', function() {
   
   let expandRefs;
   beforeEach(() => expandRefs = require('../index.js'))
   
-  testData.forEach(test => {
+  const TESTS = [
+    { title: 'simple expansion', input:'simple_input.json', expanded:'simple_output.json'},
+    { title: 'multiple references', input:'multi_references_input.json', expanded:'multi_references_output.json'},
+    { title: 'complex expansion', input:'complex_input.json', expanded:'complex_output.json'},
+  ]
+
+  TESTS.forEach(test => {
     it(test.title, () => {
-      const result = expandRefs.expanded(test.input);
-      assert.deepEqual(result, test.output)
+      const input = require(`./data/${test.input}`);
+      const expected = require(`./data/${test.expanded}`);
+      const result = expandRefs.expanded(input);
+      assert.deepEqual(result, expected)
     })
   })
 });
